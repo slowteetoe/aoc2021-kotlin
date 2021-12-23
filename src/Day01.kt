c30d09d0595e46dd939874c966fdf36c03534457
Steven Lotito <steven.lotito@gmail.com>
2021-12-23 09:04:50 -0800

    day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        val ints = input.map { it.toInt() }
+        for (n in ints.indices) {
+            if (n > 0 && ints[n] > ints[n - 1]) {
+                count++
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val ints = input.map { it.toInt() }
+        var count = 0
+        for (i in 3 until ints.size) {
+            if (ints[i] + ints[i - 1] + ints[i - 2] > ints[i - 1] + ints[i - 2] + ints[i - 3]) {
+                count++
+            }
+        }
+        return count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
