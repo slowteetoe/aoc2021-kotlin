f8f51a745a6ab04c2aa971d2d22f6cfb0bd32b01
Steven Lotito <steven.lotito@gmail.com>
2021-12-23 09:16:21 -0800

    day 2 complete

---
+++ src/Day02.kt
@@ -0,0 +1,44 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        var hpos = 0
+        var depth = 0
+        input.forEach {
+            val cmdAndAmount = it.trim().split(" ")
+            val cmd = cmdAndAmount[0]
+            val amt = cmdAndAmount[1].toInt()
+            if (cmd == "forward") {
+                hpos += amt
+            } else if (cmd == "down") {
+                depth += amt
+            } else {
+                depth -= amt
+            }
+        }
+        return hpos * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        var hpos = 0
+        var depth = 0
+        var aim = 0
+        input.forEach {
+            val cmdAndAmount = it.trim().split(" ")
+            val cmd = cmdAndAmount[0]
+            val amt = cmdAndAmount[1].toInt()
+            if (cmd == "forward") {
+                hpos += amt
+                depth += aim * amt
+            } else if (cmd == "down") {
+                aim += amt
+            } else {
+                aim -= amt
+            }
+        }
+        return hpos * depth
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
